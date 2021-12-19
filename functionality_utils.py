998dc7d856f4a4627f19ac44778e135ad193ae72
micferr <micfer96@gmail.com>
2021-12-19 19:47:50 +0100

    feat: add functionalities

--- functionality_utils.py
+++ functionality_utils.py
@@ -1,10 +1,12 @@
 # This file contains utilities specific to the homework's 4 functionalities to be implemented, e.g. for plotting data
+from typing import Optional
 
 from IPython.display import display
-from pathlib import Path
 import pandas as pd
+import networkx
 
 from graph_utils import Graph
+
 
 def functionality_1():
     """
@@ -32,3 +34,148 @@ def functionality_1():
 
     display(df)
 
+
+def functionality_2(on_graph: Optional[Graph] = None):
+    """
+    Implements the frontend for the second functionality.
+
+    :param on_graph: if provided, run the method on the argument file without requesting the user to input it.
+    """
+    if not on_graph:
+        graph_file = input("Input the path to the graph file...")
+        print(f"Graph file: {graph_file}")
+        min_timestamp = 1199142000 # Jan 1 2008, rough estimate since S/O was launched in 2008
+        max_timestamp = 1640000000 # Falls on Dec 20 2021, right after the homework's deadline
+        time_step = max_timestamp - min_timestamp
+        # Run on the graph by splitting it by equal time intervals
+        graphs = [
+            Graph(
+                graph_file,
+                from_timestamp=min_timestamp+time_step//4*i,
+                to_timestamp=min_timestamp+time_step//4*(i+1)
+            )
+            for i in range(4)
+        ]
+    else:
+        # Run on a single graph
+        graphs = [on_graph]
+
+    available_metrics = ["Betweenness", "Centrality", "Closeness", "PageRank"]
+    metric = input("Insert a metric [Betweenness, Centrality, Closeness, PageRank]...")
+
+    if metric not in available_metrics:
+        print("Metric not valid")
+        return
+    print(f"Chosen metric: {metric}")
+
+    node = int(input("Input a node to compute the metric on..."))
+    print(f"Chosen node: {node}")
+
+    for graph in graphs:
+        if metric == "Betweenness":
+            metric_value = graph.node_betweenness(node)
+        elif metric == "Centrality":
+            metric_value = graph.node_degree_centrality(node)
+        elif metric == "Closeness":
+            metric_value = graph.node_closeness(node)
+        else:
+            metric_value = graph.pagerank()[node]
+
+        nx_graph = graph.to_networkx()
+
+        # Keep only the relevant nodes to plot them
+        nodes_to_discard = [
+            n
+            for n in nx_graph.nodes
+            if not nx_graph.has_edge(node, n) and not nx_graph.has_edge(n, node)
+        ]
+        nx_graph.remove_nodes_from(nodes_to_discard)
+
+        node_colors = ["yellow" if n == node else "blue" for n in nx_graph.nodes]
+        networkx.draw_circular(nx_graph, node_color=node_colors)
+
+        print(f"{metric}: {metric_value}")
+
+
+def functionality_3(on_graph: Optional[Graph] = None):
+    """
+    Implements the frontend for the third functionality.
+
+    :param on_graph: if provided, run the method on the argument file without requesting the user to input it.
+    """
+    if not on_graph:
+        graph_file = input("Input the path to the graph file...")
+        print(f"Graph file: {graph_file}")
+        graph = Graph(graph_file)
+    else:
+        graph = on_graph
+
+    nodes = list(map(int, input("Input the space-separated list of nodes to visit (including first and last)...").split(" ")))
+    print(f"Nodes to traverse: {nodes}")
+
+    # Check that all nodes are in the graph
+    all_nodes = graph.node_ids
+    for node in nodes:
+        if node not in all_nodes:
+            print(f"{node} is not a node in the graph.")
+            return
+
+    paths = []
+    for i in range(len(nodes)-1):
+        _from, _to = nodes[i], nodes[i+1]
+        path = graph.shortest_route(_from, _to)
+
+        if path is None:
+            print("Not possible")
+            return
+
+        paths.append(path)
+
+    # Merge all paths in a single walk
+    walk = paths[0]
+    for path in paths[1:]:
+        # Remove the starting node from each subsequent path, as it is already contained in the previous one
+        walk.extend(path[1:])
+
+    nx_graph = graph.to_networkx()
+    edges = set((walk[i], walk[i+1]) for i in range(len(walk)-1))  # to use for coloring
+    edge_colors = ["red" if (u,v) in edges else "black" for u,v in nx_graph.edges()]
+    node_colors = ["yellow" if node in nodes else "blue" for node in nx_graph.nodes]
+
+    print(f"Walk: {walk}")
+
+    networkx.draw_circular(nx_graph, edge_color=edge_colors, node_size=3, node_color=node_colors)
+
+
+def functionality_4(on_graph: Optional[Graph] = None):
+    """
+    Implements the frontend for the third functionality.
+
+    :param on_graph: if provided, run the method on the argument file without requesting the user to input it.
+    """
+    if not on_graph:
+        graph_file = input("Input the path to the graph file...")
+        print(f"Graph file: {graph_file}")
+        graph = Graph(graph_file)
+    else:
+        graph = on_graph
+
+    nodes = list(map(int, input("Input the two nodes to disconnect...").split(" ")))
+    if len(nodes) != 2:
+        print("Invalid number of nodes supplied.")
+        return
+
+    print(f"Source: {nodes[0]}, Target: {nodes[1]}")
+
+    min_cut = graph.edmond_karp_min_cut(nodes[0], nodes[1])
+    if not min_cut:
+        print("The nodes are already disconnected.")
+        return
+
+    nx_graph = graph.to_networkx()
+    edge_colors = ["red" if (u, v) in min_cut else "black" for u, v in nx_graph.edges()]
+    node_colors = ["yellow" if node in nodes else "blue" for node in nx_graph.nodes]
+
+    print(f"Min cut: {min_cut}")
+
+    networkx.draw_circular(nx_graph, edge_color=edge_colors, node_size=3, node_color=node_colors)
--- graph_utils.py
+++ graph_utils.py
@@ -3,12 +3,14 @@
 from pathlib import Path
 from typing import Union, Optional
 import csv
+import networkx
 
 class Graph:
 
     adjacents: dict[int, dict[int,float]]
     is_directed: bool
     _include_self_loops: bool
+    nodes: set[int]
 
     def __init__(
             self,
@@ -38,6 +40,7 @@ class Graph:
         self.is_directed = make_directed
         self.adjacents = dict()
         self._include_self_loops = include_self_loops
+        self.nodes = set()
 
         def _add_interaction(_from, _to):
             if _from not in self.adjacents:
@@ -45,19 +48,29 @@ class Graph:
             if _to not in self.adjacents[_from]:
                 self.adjacents[_from][_to] = 0
             self.adjacents[_from][_to] += 1
+            self.nodes.add(_from)
+            self.nodes.add(_to)
 
         with open(file, "r") as file_in:
             reader = csv.reader(file_in, delimiter=" ")
             current_row = 0
             for row in reader:
                 from_user, to_user, timestamp = map(int, row)
+
                 if from_timestamp <= timestamp <= to_timestamp and (include_self_loops or from_user != to_user):
-                    _add_interaction(from_user, to_user)
-                    if not make_directed and from_user != to_user:  # Add the reciprocal edge, if it's not a self-loop
-                        _add_interaction(to_user, from_user)
+                    # This if is to store all edges (up to a point), but only if no new nodes are added
+                    if current_row < 2000 or (from_user in self.nodes and to_user in self.nodes):
+                        _add_interaction(from_user, to_user)
+                        if not make_directed and from_user != to_user:  # Add the reciprocal edge, if it's not a self-loop
+                            _add_interaction(to_user, from_user)
+
                 current_row += 1
                 if log_frequency and not (current_row % log_frequency):
                     print(f"{current_row} rows loaded...")
+                # Putting this here to make graphs in functionalities 2-4 easier to visualize
+                # Remove this if to work on the whole graph
+                if current_row == 500000:
+                    break
 
         # Edges are distances, so weights should be inverse to the link's strength. Let's use reciprocals of interaction count
         for _from in self.adjacents:
@@ -65,12 +78,11 @@ class Graph:
                 self.adjacents[_from][_to] = 1.0/self.adjacents[_from][_to]
 
 
-    def save_with_weights(self, file: Union[Path, str], nodes_to_keep: set[int]) -> None:
+    def save_with_weights(self, file: Union[Path, str]) -> None:
         """
         Save the graph to a file, keeping pre-computed weights
 
         :param file: the file to which to save the graph
-        :param nodes_to_keep: IDs of the nodes to be kept
         """
         with open(file, "w") as file_out:
             writer = csv.writer(file_out, delimiter=" ")
@@ -80,8 +92,8 @@ class Graph:
             ])
             for _from in self.adjacents:
                 for _to in self.adjacents[_from]:
-                    if _from in nodes_to_keep and _to in nodes_to_keep:
-                        writer.writerow([_from, _to, self.adjacents[_from][_to]])
+                    writer.writerow([_from, _to, self.adjacents[_from][_to]])
+
 
     @staticmethod
     def load_with_weights(cls, file: Union[Path, str]) -> "Graph":
@@ -106,6 +118,25 @@ class Graph:
                 g.adjacents[from_user][to_user] = weight
                 current_row += 1
         return g
+
+    def to_networkx(self) -> networkx.Graph:
+        """
+        Convert the graph to NetworkX for drawing
+
+        :return: the graph as a networkx.Graph
+        """
+        g = networkx.Graph() if not self.is_directed else networkx.DiGraph()
+
+        for node_from, node_adjacents in self.adjacents.items():
+            for node_to, weight in node_adjacents.items():
+                if node_from not in g:
+                    g.add_node(node_from)
+                if node_to not in g:
+                    g.add_node(node_to)
+                g.add_weighted_edges_from([(node_from, node_to, weight)])
+
+        return g
+
 
     def __len__(self) -> int:
         """
@@ -171,6 +202,232 @@ class Graph:
         if not self.is_directed:
             d *= 2
         return d
+
+
+    def shortest_route(self, _from, _to) -> Optional[list[int]]:
+        """
+        Returns the shortest route between two nodes.
+
+        :param _from: Starting node
+        :param _to: End node
+        :return: List of nodes making up the shortest path between the two nodes. It returns None if no path exists
+        """
+        from queue import PriorityQueue
+
+        q = PriorityQueue()
+        q.put((0, _from))  # The queue will contain (total_distance_from_`_from`, node_id) tuples,
+
+        parents = dict()
+        visited = set()
+        distances = {_from: 0}
+        while not q.empty():
+            min_dist, current_node = q.get()
+            visited.add(current_node)
+
+            if current_node == _to:
+                break
+
+            for adjacent in self.adjacents.get(current_node, dict()):
+                if adjacent in visited:
+                    continue
+
+                adjacent_distance = distances[current_node] + self.adjacents[current_node][adjacent]
+                if not adjacent in distances or adjacent_distance < distances[adjacent]:
+                    distances[adjacent] = adjacent_distance
+                    parents[adjacent] = current_node
+                    q.put((adjacent_distance, adjacent))
+
+        if _to not in visited:
+            return None
+
+        path = [_to]
+        current_node = _to
+        while current_node != _from:
+            current_node = parents[current_node]
+            path.append(current_node)
+        return list(reversed(path))
+
+
+    def simple_path(self, source: int, target: int) -> Optional[list[int]]:
+        """
+        Given two nodes, returns a path between the two (with no necessary features, such as being the shortest).
+
+        :param source: The source node
+        :param target: The target node
+        :return: A path between the two nodes, or None if one is not found
+        """
+        from queue import Queue
+
+        queue = Queue()
+        parents = {source: source}
+        visited = {source}
+        while not queue.empty():
+            node = queue.get()
+
+            for adjacent in self.adjacents[node]:
+                if adjacent not in visited:
+                    visited.add(adjacent)
+                    parents[adjacent] = node
+                    queue.put(adjacent)
+
+        if target not in visited:
+            return None
+
+        path = [target]
+        current_node = target
+        while current_node != source:
+            current_node = parents[current_node]
+            path.append(current_node)
+
+        return list(reversed(path))
+
+
+    def edmond_karp_min_cut(self, source: int, target: int) -> list[tuple[int, int]]:
+        """
+        Return the min-cut from source to target using the Edmond-Karp algorithm, modified to solve the problem
+        in functionality 4.
+
+        For simplicity, note that the graph is modified with its reduced version as the algorithm runs, and at the end
+        modified edges are restored.
+
+        :param source: The source node
+        :param target: The target node
+        :return: The list of edges making up the min-cut
+        """
+        from copy import deepcopy
+
+        adj_copy = deepcopy(self.adjacents)
+
+        min_cut = []
+        while True:
+            # path = self.simple_path(source, target)
+            path = self.shortest_route(source, target)
+            if not path:
+                self.adjacents = adj_copy
+                return min_cut
+
+            edges = [(path[i], path[i+1]) for i in range(len(path)-1)]
+            min_edge_weight = min([self.adjacents[_from][_to] for _from, _to in edges])
+            min_edge = [(_from, _to) for _from, _to in edges if self.adjacents[_from][_to] == min_edge_weight][0]
+
+            del self.adjacents[min_edge[0]][min_edge[1]]
+
+            min_cut.append(min_edge)
+
+    def node_degree_centrality(self, node: int) -> int:
+        """
+        Return the node degree centrality. For directed graphs, it's defined as the sum
+        of in-degree and out-degree.
+
+        :param node: the node to compute centrality for.
+        :return: The node's degree centrality
+        """
+        out_degree = len(self.adjacents[node])
+        if not self.is_directed:
+            return out_degree
+
+        in_degree = len([k for k,v in self.adjacents.items() if node in v])
+        return out_degree + in_degree
+
+    def node_betweenness(self, node: int) -> float:
+        """
+        Return the node's betweenness, defined as the ratio of shortest paths across all node pairs
+        that pass through the given node.
+
+        :param node: The node to compute betweenness for
+        :return: The node's betweenness
+        """
+        count = 0
+        for node_i in self.node_ids:
+            for node_j in self.node_ids:
+                if node in [node_i, node_j]:
+                    continue
+
+                shortest_path = self.shortest_route(node_i, node_j)
+                if node in shortest_path:
+                    count += 1
+
+        return count/(len(self.node_ids)*len(self.node_ids)-1)
+
+    def node_closeness(self, node: int):
+        """
+        Return the node's closeness, defined as the reciprocal of the sum of distances from all other nodes
+
+        :param node: The node to compute closeness for
+        :return: The node's closeness
+        """
+        total_distance = 0
+        for source_node in self.node_ids:
+            if node == source_node:
+                continue
+
+            distance = len(self.shortest_route(source_node, node))-2 # -2 to remove source and target
+            total_distance += distance
+
+        return 1.0/total_distance
+
+    def pagerank(self, num_iterations: int = 100) -> dict[int, float]:
+        """
+        Returns the PageRank values for all nodes. The algorithm is run for a predefined number of iterations.
+
+        :param num_iterations: Number of items iterations
+        :return: PageRank for all nodes in the graph
+        """
+        nodes = self.node_ids
+        num_nodes = len(nodes)
+        pr = {node: 1/num_nodes for node in nodes}
+        damping = 0.85
+
+        # Pre-compute in-links for performance
+        in_links: dict[int, set] = {node: set() for node in self.nodes}
+        for node, adjacents in self.adjacents.items():
+            in_links[node].update(adjacents)
+
+        for i in range(num_iterations):
+            pr_next = dict()
+            for node in nodes:
+                pr_next[node] = ((1-damping)/num_nodes) + damping*sum([pr[in_link]/len(self.adjacents[in_link]) for in_link in in_links[node]])
+            pr = pr_next
+
+        return pr
+
+
+def example_graph() -> Graph:
+    """
+    Return a simple, predefined graph to provide examples of visualizations
+
+    :return: A simple, fully-connected graph
+    """
+    g = Graph(file=None)
+    g.nodes = set(range(10))
+    g.adjacents = {i: dict() for i in range(10)}
+    for i in range(10):
+        for j in range(10):
+            if i != j:
+                g.adjacents[i][j] = (i-j)**2
+    g.is_directed = True
+    g._include_self_loops = True
+    return g
+
+
+def random_graph(num_nodes: int, edge_probability: float) -> Graph:
+    """
+    Returns a random graph,
+
+    :return:
+    """
+    from random import uniform
+
+    g = Graph(file=None)
+    g.nodes = set(range(num_nodes))
+    g.adjacents = {i: dict() for i in range(num_nodes)}
+    for i in range(num_nodes):
+        for j in range(num_nodes):
+            if i != j and uniform(0, 1) < edge_probability:
+                g.adjacents[i][j] = 1
+    g.is_directed = True
+    g._include_self_loops = True
+    return g
 
 
 if __name__ == "__main__":
